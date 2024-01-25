0157071ca8a4b0a4f0ff2e243f1bfc424471d806
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-25 21:15:47 +0100

    solved

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action :set_list, only: [:new, :create, :destroy]
+  before_action :set_list, only: [:new, :create]
 
   def new
     @bookmark = Bookmark.new(list: @list)
@@ -9,16 +9,17 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to lists_path
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
-      raise
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list.id
     @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
   end
 
   private
